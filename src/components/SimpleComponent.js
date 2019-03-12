600cff9f9f952b3b6086dd857f41a40989bb5f43
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-12 22:04:30 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,15 @@
-// Code SimpleComponent Here
+class SimpleComponent extends Component {
+  constructor() {
+    super() ;
+    this.state = {
+      mood: 'happy'
+    }
+    render() {
+      return (
+        <div>
+          {this.state.mood}
+        </div>
+      )
+    }
+  }
+}
