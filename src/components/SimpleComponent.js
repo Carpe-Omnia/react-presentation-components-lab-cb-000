60d245b64a2b16645b179f1998bae696a40b2950
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-12 22:07:12 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -4,12 +4,17 @@ class SimpleComponent extends Component {
     this.state = {
       mood: 'happy'
     }
+    this.handleClick = this.handleClick.bind(this) ;
+  }
+  handleClick() {
+    this.state.mood === 'happy' ? this.setState( {mood: 'sad'} ) : this.setState ({mood: 'happy'})
+  }
     render() {
       return (
-        <div>
+        <div onClick={this.handleClick}>
           {this.state.mood}
         </div>
       )
     }
-  }
+  
 }
