519eae2273600f5aeff4a0419da7871254695a15
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-12 22:17:43 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -4,7 +4,7 @@ class SimplerComponent extends Component {
   constructor() {
     this.changeText = this.changeText.bind(this)
   }
-  var moodText = "I am just happy" ;
+  const moodText = "I am just happy" ;
   changeText() {
     moodText = "I am no longer as happy" ;
   }
